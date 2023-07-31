4dda87e49ff211b01411d48ad6cac134151f31c1
boogh <130850411+boogh46@users.noreply.github.com>
2023-07-31 22:17:05 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x3ba69e45365112466ecd00e0ee4e83a58d069929";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
